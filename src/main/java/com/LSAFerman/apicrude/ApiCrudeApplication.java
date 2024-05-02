a6a23e4c2f917bbec0683485786d0becc11c50a0
Leticia_Alves <DTI.Leticia.Alves@segurorsura.com.br>
2024-05-02 14:37:09 -0300

    Initial commit

---
+++ src/main/java/com/LSAFerman/apicrude/ApiCrudeApplication.java
@@ -0,0 +1,13 @@
+package com.LSAFerman.apicrude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LSAFerman/apicrude/ApiCrudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LSAFerman.apicrude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
